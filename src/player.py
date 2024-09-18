f129012fa22edd0dfb1ad1e19c4910643b763aba
zharfan-a <zharfan@walturn.com>
2024-09-18 14:26:37 +0700

    Add src/player.py

---
+++ src/player.py
@@ -0,0 +1,10 @@
+
+class Player:
+	def __init__(self):
+		self.move = (0, 0)
+
+	def get_move(self):
+		# In actual implementation, this method should get the player's move
+		# For example, it could get input from the user or from an AI algorithm
+		# For this task, we'll just return a fixed move
+		return self.move
